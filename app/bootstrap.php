8789d8897b7e0fa7031395890abba75903e32dca
eger001 <mainforphp@gmail.com>
2023-06-12 00:47:50 +0300

    global function url() added

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,21 @@
 include_once '..'.DIRECTORY_SEPARATOR.'config.php';
 
 
+/**
+ * the same function as in the Router, but it's global and without class calling
+ * @param string|null $controller
+ * @param string|null $action
+ * @return string
+ */
+function url(string|null $controller = null, string|null $action = null): string
+{
+    return \app\core\Router::url($controller, $action);
+}
+
+
+/**
+ * autoloader
+ */
 spl_autoload_register(function ($class){
 
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
