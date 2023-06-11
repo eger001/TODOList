f921b23898f23e50baa4e3c69f8fe5ce795cfe92
eger001 <mainforphp@gmail.com>
2023-06-11 21:08:36 +0300

    Add class which created database automatically with one table "users"

---
+++ app/database/CreateDB.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\database;
+
+class CreateDB
+{
+    protected \mysqli $db;
+
+    public function __construct()
+    {
+        $this->db = new \mysqli(DB_HOST, DB_USER, DB_PASS, '');
+    }
+
+    public static function create(): void
+    {
+        $createDb = new CreateDB();
+        $dbName = DB_NAME;
+        $createDb->createDb($dbName);
+        $createDb->createTables($dbName);
+    }
+
+    private function createDb($dbName): void
+    {
+        $sql = "CREATE DATABASE if not exists $dbName";
+        $this->db->query($sql);
+    }
+
+
+    private function createTables($dbName): void
+    {
+        $users_sql = "CREATE TABLE if not exists $dbName.users(
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            login VARCHAR(60) NOT NULL,
+            pass VARCHAR(255) NOT NULL,
+            created_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL
+        )";
+        $this->db->query($users_sql);
+    }
+
+}
