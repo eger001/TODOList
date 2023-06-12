1f7358487b7c0f888ac7da545891593288f83784
eger001 <mainforphp@gmail.com>
2023-06-13 01:43:41 +0300

    get() realized for full information about user

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -6,6 +6,8 @@ use app\core\Model;
 
 class UserModel extends Model
 {
+    private string $table = 'users';
+
     public function add(array $user): void
     {
         $user['email'] = $user['email'];
@@ -15,5 +17,15 @@ class UserModel extends Model
            VALUES ('{$user['email']}', '{$user['pass']}');";
         $this->db->query($sql);
         //TODO validate!
+    }
+
+
+    public function get(string $email): array|null
+    {
+        $stmt = $this->db->prepare("SELECT * FROM $this->table WHERE users.login = ? LIMIT 1");
+        $stmt->bind_param('s', $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
     }
 }
