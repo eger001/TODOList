9aef0cd8ae05ca4546e47abc00ed835f1953fd2e
eger001 <mainforphp@gmail.com>
2023-06-13 01:44:10 +0300

    validatePass() init, not finish version

--- app/core/Validator.php
+++ app/core/Validator.php
@@ -33,6 +33,34 @@ class Validator
             if (count($emailParts[1]) != 2 || empty($emailParts[1][0] || empty($emailParts[1][1]))) {
                 $this->errors[] = 'Incorrect email';
             }
+            //TODO validation with db
+        }
+
+        //TODO react to two validation in one time
+        return $this->errors;
+    }
+
+
+    public function validatePass($pass): array
+    {
+        if (empty($pass))
+        {
+            $this->errors[] = 'Password cannot be empty';
+        } else if(strlen($pass) > 20 || strlen($pass) < 8)
+        {
+            $this->errors[] = 'Password must be at least 8 characters and not more than 20';
+        } else if (!preg_match('/^[a-zA-Z0-9]+$/', $pass))
+        {
+            $this->errors[] = 'The password can only contain numbers and Latin letters';
+        } else if (!preg_match('/[A-Z]/', $pass))
+        {
+            $this->errors[] = 'Password must contain at least one uppercase letter';
+        } else if (!preg_match('/[a-z]/', $pass))
+        {
+            $this->errors[] = 'Password must contain at least one lowercase letter';
+        } else if (!preg_match('/[0-9]/', $pass))
+        {
+            $this->errors[] = 'Password must contain at least one number';
         }
         return $this->errors;
     }
