cef791257c7c5c42412829812d3c8c7a2b9e9108
eger001 <mainforphp@gmail.com>
2023-06-23 01:48:50 +0300

    some method refactoring (using trait)

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -7,46 +7,35 @@ use app\core\Router;
 use app\core\Session;
 use app\core\Validator;
 use app\models\UserModel;
+use app\traits\UserInputData;
+use JetBrains\PhpStorm\NoReturn;
 
 class UserController extends Controller
 {
-
     public function __construct($template = null)
     {
         parent::__construct($template);
         $this->model = new UserModel();
-        $this->validator = new Validator();
     }
 
     public function index(): void
     {
         $errors = Session::all('authorization');
-        $this->view->render('authorization',
+        $this->view->render('start_page',
             [
                 'errors'=>$errors,
             ]);
     }
 
-    public function store():void
+
+    /**
+     * method which store new user in DB and use a trait methods
+     * @return void
+     */
+    #[NoReturn] public function store():void
     {
-        $user = filter_input_array(INPUT_POST,
-            [
-                'email'=>FILTER_DEFAULT,
-                'pass'=>FILTER_DEFAULT,
-            ]
-        );
-        $errors = $this->validator->validateUserEmail($user['email']);
-        if (count($errors)>0)
-        {
-            Session::save('authorization', $errors);
-            Router::goBack();
-        }
-        $errors = $this->validator->validatePass($user['pass'], $user['email']);
-        if (count($errors)>0)
-        {
-            Session::save('authorization', $errors);
-            Router::goBack();
-        }
-        $this->model->add($user);
+        $this->model->add($this->authorizationAlgo());
+
+        Router::redirect('user','index');
     }
 }
