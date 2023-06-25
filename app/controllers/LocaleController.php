9b6b9553b86529e272f99b826914253fff85fb20
eger001 <mainforphp@gmail.com>
2023-06-25 20:36:11 +0300

    Changeable interface implemented

--- app/controllers/LocaleController.php
+++ app/controllers/LocaleController.php
@@ -5,12 +5,11 @@ namespace app\controllers;
 use app\core\Controller;
 use app\core\Router;
 use app\core\Session;
+use app\interfaces\Changeable;
 use JetBrains\PhpStorm\NoReturn;
 
-class LocaleController extends Controller
+class LocaleController implements Changeable
 {
-    public function index()
-    {}
     #[NoReturn] public function change(): void
     {
         $localeData = filter_input_array(INPUT_POST)['locale'];
